4d970987d31cfbebe979d249aedff709ba5b39b1
yaxshioken <yaxshioken@gmail.com>
2025-06-10 18:57:22 +0500

    amend

---
+++ 1ModulTakrorlash/1topshiriq.py
@@ -0,0 +1,12 @@
+#1. Foydalanuvchining ism va familiyasini qabul qiladigan
+# va ularni teskari tartibda ular orasida bo'sh joy qoldirib chop etadigan
+# Python dasturini yozing.
+Ism = str(input("Ismingizni kiriting = > "))
+Familiya = str(input("Familiyangizni kiriting = > "))
+print(Familiya + " " + Ism)
+
+
+#2. Kun boshlangandan keyin (00:00) N sekund o’tdi, yani N sekundni kiritamiz.
+#Kun boshlangandan keyin nechinchi soatda o’tganligini aniqlang
+
+Sekund=int(input("Sekundni kiriting -> "))
